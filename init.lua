4af1290fbcbe2f627f1b2fbbe9b0a7ccd4a36080
Liam Gensel <liam@gensel.dev>
2025-11-27 10:32:16 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -902,40 +902,36 @@ require('lazy').setup({
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
+    'nvim-mini/mini.nvim',
+      config = function()
       -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
       -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- FORCE no icons for the statusline
+      statusline.setup { use_icons = false }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- ... (Statusline section_location config remains the same) ...
 
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
+      -- SETUP MINI.PICK
+      require('mini.pick').setup({
+        -- Disable icons in the picker
+        window = {
+            config = { border = 'double' } -- Optional: nice border without icons
+        }
+      })
+      
+      -- Keymaps for Mini Pick
+      vim.keymap.set('n', '<leader>ff', function() require('mini.pick').builtin.files() end, { desc = 'Find Files' })
+      vim.keymap.set('n', '<leader>bb', function() require('mini.pick').builtin.buffers() end, { desc = 'Find Buffers' })
+      
+      -- Optional: Replace other Telescope mappings with Mini Pick equivalents if you wish
+      vim.keymap.set('n', '<leader>sg', function() require('mini.pick').builtin.grep_live() end, { desc = 'Search Grep' })
+      vim.keymap.set('n', '<leader>sh', function() require('mini.pick').builtin.help() end, { desc = 'Search Help' })
     end,
   },
   { -- Highlight, edit, and navigate code
